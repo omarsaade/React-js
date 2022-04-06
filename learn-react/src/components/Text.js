4aeb81f670b560f62f5642bcb5c934cdd6906346
omar saade <omarsaadeh11@gmail.com>
2022-04-07 02:03:27 +0300

    React

--- learn-react/src/components/Text.js
+++ learn-react/src/components/Text.js
@@ -3,23 +3,27 @@ import React from 'react';
 function Text() {
     return (
         // <p>Grapic and web design are far more than a job</p>
-        <div>
-            <form className='form-check '>
-                <label>
-                    <input type="checkbox" name="name" />
-                    Facebook
-                </label><br></br>
-                <label>
-                    <input type="checkbox" name="name" />
-                    Youtube
-                </label><br></br>
-                <label>
-                    <input type="checkbox" name="name" />
-                    Instagram
-                </label><br></br>
-                <input type="submit" value="Submit" />
-            </form>
-        </div>
+        // <div>
+        //     <form className='form-check '>
+        //         <label>
+        //             <input type="checkbox" name="name" />
+        //             Facebook
+        //         </label><br></br>
+        //         <label>
+        //             <input type="checkbox" name="name" />
+        //             Youtube
+        //         </label><br></br>
+        //         <label>
+        //             <input type="checkbox" name="name" />
+        //             Instagram
+        //         </label><br></br>
+        //         <input type="submit" value="Submit" />
+        //     </form>
+        // </div>
+        <p>Graphic and web design are far more than a job for me.Thanks to my extensive
+            technical knowledge , I can implement my creative ideas very well,so that I can
+            offer tailor-made solutions for customers.
+        </p>
     );
 }
 
