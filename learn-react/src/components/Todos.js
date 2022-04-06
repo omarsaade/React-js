c3dd3cbca04aeeea85d903a8302643328aa61083
omar saade <omarsaadeh11@gmail.com>
2022-04-06 21:32:22 +0300

    React

---
+++ learn-react/src/components/Todos.js
@@ -0,0 +1,13 @@
+import React ,{Component} from 'react';
+
+
+
+class Todos extends Component {
+  render(){
+    return (
+      <h1>My Todos</h1>
+    );
+  }
+}
+
+export default Todos;
---
+++ learn-react/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';//hol el satren lezem yenwejdo deyman / ne7na mne7tej la maktebten /maktabe react w maktabet ReactDOm
+import ReactDOM from 'react-dom';//hol el satren lezem yenwejdo deyman li2an kelmet dom bta3ni el saf7a
+import App from './App';
+
+//fina nekteba hon degre badal el export import
+// function App() {
+// return (
+//     <h1>Test</h1>     
+// );
+// }
+
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+
+
