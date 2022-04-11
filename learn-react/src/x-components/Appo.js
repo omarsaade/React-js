1b500cf1960a0c37ccb5aefee2d7cf1253637e6f
omar saade <omarsaadeh11@gmail.com>
2022-04-11 18:41:44 +0300

    React

--- learn-react/src/x-components/Appo.js
+++ learn-react/src/x-components/Appo.js
@@ -1,25 +1,79 @@
-import React, { Component } from 'react';
-import Header from './Header';
-import Text from './Text';
-import Social from './Social';
-import Pic from './Pic';
+import React, { Component } from "react";
 
 
+class Appo extends Component {
 
-
-class App extends Component {
   render() {
     return (
-      // jsx
-      <div className='container' >
-        <Header />
-        <Text />
-        <Social />
-        <Pic />
-      </div>
-    );
+      <div>Hello</div>
+    )
+
+
   }
+
+
 
 }
 
-export default App;
+export default Appo
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React, { Component } from 'react';
+// import Header from './Header';
+// import Text from './Text';
+// import Social from './Social';
+// import Pic from './Pic';
+
+
+
+
+// class App extends Component {
+//   render() {
+//     return (
+//       // jsx
+//       <div className='container' >
+//         <Header />
+//         <Text />
+//         <Social />
+//         <Pic />
+//       </div>
+//     );
+//   }
+
+// }
+
+// export default App;
